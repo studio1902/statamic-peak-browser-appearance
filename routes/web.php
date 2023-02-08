f18c2138e25e4c05a3e73dda56b833b2a85d8e91
Marco Rieser <marco@rieser.dev>
2023-02-08 09:05:09 +0100

    extract view for `manifest.json`

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 // The Manifest route to the manifest.json
-Route::statamic('/site.webmanifest', 'manifest/manifest', [
+Route::statamic('/site.webmanifest', 'statamic-peak-browser-appearance::manifest/manifest', [
     'layout' => null,
     'content_type' => 'application/json'
 ]);
