fe02ce49ae02a54932ddf89cfcf0c75a339f0003
Marco Rieser <marco@rieser.dev>
2023-02-08 09:14:37 +0100

    Make views publishable

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -13,6 +13,7 @@ class ServiceProvider extends AddonServiceProvider
     public function bootAddon()
     {
         $this->registerPublishableFieldsets();
+        $this->registerPublishableViews();
     }
 
     protected function registerPublishableFieldsets()
@@ -20,5 +21,12 @@ class ServiceProvider extends AddonServiceProvider
         $this->publishes([
             __DIR__ . '/../resources/fieldsets' => resource_path('fieldsets/vendor/statamic-peak-browser-appearance'),
         ], 'statamic-peak-browser-appearance-fieldsets');
+    }
+
+    protected function registerPublishableViews()
+    {
+        $this->publishes([
+            __DIR__ . '/../resources/views' => resource_path('views/vendor/statamic-peak-browser-appearance'),
+        ], 'statamic-peak-browser-appearance-views');
     }
 }
