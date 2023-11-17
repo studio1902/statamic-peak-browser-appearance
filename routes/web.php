a3a7ac26d135acb00a8248d8b7a7aa1dac40996c
Marco Rieser <marco@rieser.dev>
2023-11-17 10:06:43 +0100

    enable site.webmanifest per site

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,15 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Statamic\Facades\Site;
+use Statamic\Facades\URL;
 
-// The Manifest route to the manifest.json
-Route::statamic('/site.webmanifest', 'statamic-peak-browser-appearance::manifest/manifest', [
-    'layout' => null,
-    'content_type' => 'application/json'
-]);
+Site::all()->each(function (\Statamic\Sites\Site $site) {
+    $relativeSiteUrl = URL::makeRelative($site->url());
+
+    // The Manifest route to the manifest.json
+    Route::statamic(URL::tidy($relativeSiteUrl . '/site.webmanifest'), 'statamic-peak-browser-appearance::manifest/manifest', [
+        'layout' => null,
+        'content_type' => 'application/json'
+    ]);
+});
