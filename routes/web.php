a74b9307f5f529bc635d001f3818b1ce3b841e36
Marco Rieser <marco@rieser.dev>
2023-11-28 10:33:56 +0100

    make it beautiful

--- routes/web.php
+++ routes/web.php
@@ -4,14 +4,14 @@ use Illuminate\Support\Facades\Route;
 use Statamic\Facades\Site;
 use Statamic\Facades\URL;
 
-$sites = Site::all();
-$sitesCount = $sites->count();
-
-$sites->each(function (\Statamic\Sites\Site $site) use ($sitesCount) {
+Site::all()->each(function (\Statamic\Sites\Site $site) {
     $relativeSiteUrl = URL::makeRelative($site->url());
 
     // The Manifest route to the manifest.json
-    $manifest = $sitesCount === 1 ? 'site.webmanifest' : $site->handle() . '/site.webmanifest';
+    $manifest = Site::all()->count() === 1
+        ? 'site.webmanifest'
+        : $site->handle() . '/site.webmanifest';
+
     Route::statamic(URL::tidy($relativeSiteUrl . '/' . $manifest), 'statamic-peak-browser-appearance::manifest/manifest', [
         'layout' => null,
         'content_type' => 'application/json'
