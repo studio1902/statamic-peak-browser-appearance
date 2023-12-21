8c188f58b871edbd78ae0a9d66955008f424f24b
Marco Rieser <marco@rieser.dev>
2023-12-21 14:31:00 +0100

    prevent manifest routes from having double handles (#12)

--- routes/web.php
+++ routes/web.php
@@ -3,16 +3,17 @@
 use Illuminate\Support\Facades\Route;
 use Statamic\Facades\Site;
 use Statamic\Facades\URL;
+use Stringy\StaticStringy as Stringy;
 
 Site::all()->each(function (\Statamic\Sites\Site $site) {
     $relativeSiteUrl = URL::makeRelative($site->url());
 
     // The Manifest route to the manifest.json
-    $manifest = Site::all()->count() === 1
-        ? 'site.webmanifest'
-        : $site->handle() . '/site.webmanifest';
+    $manifestUrl = Site::all()->count() === 1
+        ? $relativeSiteUrl . '/site.webmanifest'
+        : Stringy::ensureRight($relativeSiteUrl, '/' . $site->handle()) . '/site.webmanifest';
 
-    Route::statamic(URL::tidy($relativeSiteUrl . '/' . $manifest), 'statamic-peak-browser-appearance::manifest/manifest', [
+    Route::statamic(URL::tidy($manifestUrl), 'statamic-peak-browser-appearance::manifest/manifest', [
         'layout' => null,
         'content_type' => 'application/json'
     ])->name('manifest.' . $site->handle());
